1bf1ac8dc5d297283df63152d9e33985ffe8ca84
BJKill <brandon.j.kill@gmail.com>
2020-06-18 15:34:38 -0400

    Copied sample code and this is first attempt at adjusting it for finding and caching matrix inverses in lieu of vector means.

--- ProgrammingAssignment2-master/cachematrix.R
+++ ProgrammingAssignment2-master/cachematrix.R
@@ -3,13 +3,30 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x1 = matrix()) {
+        m1 <- NULL
+        setmat <- function(y1) {
+                x1 <<- y1
+                m1 <<- NULL
+        }
+        getmat <- function() x1
+        setsolve <- function(solve) m1 <<- solve
+        getsolve <- function() m1
+        list(setmat = setmat, getmat = getmat, setsolve = setsolve, getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x1, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m1 <- x1$getsolve()
+        if(!is.null(m1)) {
+                message("getting cached data")
+                return(m1)
+        }
+        data <- x1$getmat()
+        m1 <- solve(data, ...)
+        x1$setmean(m1)
+        m1
 }
